c999d55dc81db6a18eb3ea042f02a7a9ff0f5110
오승현 <seungheyon.oh@altiall.com>
2023-06-30 18:37:14 +0900

    .

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,10 +1,23 @@
+import dayjs from "dayjs";
+import 'dayjs/locale/ko';
 import DefaultLayout from "@/components/DefaultLayout";
+import {DatePicker} from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const Users = () => {
+
     return (
         <DefaultLayout>
             <div>
                 <h1>Users</h1>
+                <div>
+                    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'ko'} >
+                        <DatePicker label={'일자'}
+                                    format={'YYYY-MM-DD'}
+                                    defaultValue={dayjs(new Date())}/>
+                    </LocalizationProvider>
+                </div>
             </div>
         </DefaultLayout>
     )
